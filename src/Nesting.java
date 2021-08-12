e965cbd7b704dcbd13c60b2392fdae633146ad8c
Kien Nguyen <kuber@Kiens-Mac-mini.local>
2021-08-12 11:07:34 +0700

    refactor

--- src/Nesting.java
+++ src/Nesting.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Nesting {
+import java.util.Stack;
+
+public class Nesting {
+    public int solution(String S) {
+        if (S.isEmpty()) return 1;
+        if (S.charAt(0) == ')') return 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (c == '(') stack.push(c);
+            else if (stack.isEmpty()) {
+                return 0;
+            } else {
+                stack.pop();
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
 }
